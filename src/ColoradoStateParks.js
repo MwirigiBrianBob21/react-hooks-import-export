baca4f0d19eaf7b336577233595351ed1ee1e193
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-09-20 15:44:14 +0300

    Complete assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,30 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+import * as RMFunctions from "./parks/RockyMountain";
 
-  return <h1>Colorado State Parks!</h1>;
+
+export default function ColoradoStateParks() {
+  return(
+    <div>
+      <MesaVerde />
+    </div>
+  )
 }
+// import { trees, wildlife} from "./parks/RockyMountain"
+// console.log(trees)
+// wildlife()
+
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildlife,
+// } from "./parks/RockyMountain"
+
+// function ColoradoStateParks() {
+//   howManyParks(); // => "42 parks!"
+
+//   return <h1>Colorado State Parks!</h1>;
+          
+// }
+// export default ColoradoStateParks;
